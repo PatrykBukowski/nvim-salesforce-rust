660b40acd3f8d9593e413327cb5d65cbaf1a8616
PatrykBukowski <patrykbukowski96@gmail.com>
2024-02-12 18:32:19 +0100

    Test

--- lua/core/plugin_config/git.lua
+++ lua/core/plugin_config/git.lua
@@ -1,3 +1,7 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
 vim.keymap.set("n", "<leader>gc", vim.cmd.Gdiffsplit)
+vim.keymap.set("n", "<leader>gP", function()
+  local message = vim.fn.input("Message: ")
+  vim.cmd('!git add . && git commit -m"' .. message .. '" && git push')
+end)
