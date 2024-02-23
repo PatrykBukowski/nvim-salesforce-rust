cef8ae0676fcb8b2f1c40a5b313c499e54d8dcfe
PatrykBukowski <patrykbukowski96@gmail.com>
2024-02-23 12:49:38 +0100

    Change rust commands

--- lua/core/plugin_config/rust.lua
+++ lua/core/plugin_config/rust.lua
@@ -1,14 +1,14 @@
 local Terminal = require("toggleterm.terminal").Terminal
-vim.keymap.set("n", "<leader>cb", ":!cargo build<CR>")
-vim.keymap.set("n", "<leader>ch", ":!cargo check<CR>")
-vim.keymap.set("n", "<leader>cr", ":!cargo build --release<CR>")
-vim.keymap.set("n", "<leader>cf", ":!cargo fix --allow-dirty<CR>")
+vim.keymap.set("n", "<leader>Cb", ":!cargo build<CR>")
+vim.keymap.set("n", "<leader>Ch", ":!cargo check<CR>")
+vim.keymap.set("n", "<leader>Cr", ":!cargo build --release<CR>")
+vim.keymap.set("n", "<leader>Cf", ":!cargo fix --allow-dirty<CR>")
 
 local cargo_run = Terminal:new({ cmd = "cargo run", hidden = true, close_on_exit = false })
 function _cargo_run()
 	cargo_run:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>cc", "<cmd>lua _cargo_run()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>c", "<cmd>lua _cargo_run()<CR>", { noremap = true, silent = true })
 
 vim.g.rustfmt_autosave = 1
